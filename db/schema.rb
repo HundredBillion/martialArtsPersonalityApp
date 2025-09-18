6272deda958f2cf6d249497383b65352315af504
hundredbillion <david@creatio.works>
2025-09-18 16:22:24 -0400

    Update database schema with quiz tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_08_211125) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_17_000300) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "text", null: false
+    t.integer "personality", default: 0, null: false
+    t.integer "position", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["personality"], name: "index_questions_on_personality"
+    t.index ["position"], name: "index_questions_on_position"
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.integer "submission_id", null: false
+    t.integer "question_id", null: false
+    t.integer "value", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_responses_on_question_id"
+    t.index ["submission_id"], name: "index_responses_on_submission_id"
+  end
+
+  create_table "submissions", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "ip_address"
+    t.string "user_agent"
+    t.string "result_primary"
+    t.json "result_payload"
+    t.datetime "completed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "responses", "questions"
+  add_foreign_key "responses", "submissions"
 end
