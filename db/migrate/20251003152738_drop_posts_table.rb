1eedda0008a201ca2ae0a0f15b04db562c0cd2df
david lee <david@creatio.works>
2025-10-06 18:37:16 -0400

    fix migration file

--- db/migrate/20251003152738_drop_posts_table.rb
+++ db/migrate/20251003152738_drop_posts_table.rb
@@ -1,6 +1,6 @@
 class DropPostsTable < ActiveRecord::Migration[8.0]
   def change
-    drop_table :posts do |t|
+    drop_table :posts, if_exists: true do |t|
       t.string "title"
       t.text "body"
       t.datetime "created_at", null: false
